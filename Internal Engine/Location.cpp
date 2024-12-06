e718d80d9f791b0ee3341294c4853f768ba8ba55
Aleksey Morozov <a_morozov2@lesta.group>
2024-12-06 11:02:10 +0300

    создается плейн

--- Internal Engine/Location.cpp
+++ Internal Engine/Location.cpp
@@ -14,24 +14,26 @@ GameObject* Instantiate(const XMFLOAT3& pos, const XMFLOAT3& rot, const XMFLOAT3
 
 Location::Location() {
     for (int i = 0; i < 5; ++i) {
-        GameObject* obj = Instantiate(XMFLOAT3((float)i * 3, 0.0f, 0.0f),
-            XMFLOAT3(0.0f, 0.0f, 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f));
+        for (int y = 0; y < 5; ++y) {
+            GameObject* obj = Instantiate(XMFLOAT3((float)i * 2, 0.0f, y * 2),
+                XMFLOAT3(0.0f, 0.0f, 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f));
 
-        MeshComponent* mesh = obj->AddComponent<MeshComponent>();
-        mesh->setMatrix(obj->transform().getWorldMatrix());
-        engine.addMeshRenderer(mesh);
+            MeshComponent* mesh = obj->AddComponent<MeshComponent>();
+            mesh->setMatrix(obj->transform().getWorldMatrix());
+            engine.addMeshRenderer(mesh);
 
-        Insert(obj);
+            Insert(obj);
+        }
     }
 }
 
 void Location::Update(float deltaTime) {
-    static float rot = 0.0f;
-    rot += deltaTime * 5.0f;
-
-    for (GameObject* obj : m_objects) {
-        obj->setRotation(XMFLOAT3(rot, rot, rot));
-    }
+    //static float rot = 0.0f;
+    //rot += deltaTime * 5.0f;
+    //
+    //for (GameObject* obj : m_objects) {
+    //    obj->setRotation(XMFLOAT3(rot, rot, rot));
+    //}
 }
 
 void Location::Insert(GameObject* obj) {
